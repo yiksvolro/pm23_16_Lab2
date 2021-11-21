f044c90d8d7fe031e05142b27a6084c9e90971d4
yiksvolro <oleksandr.orlovskii.pm.2020@lpnu.ua>
2021-11-21 21:51:09 +0200

    jquery lib and AJAX

--- app/js/script.js
+++ app/js/script.js
@@ -3,4 +3,46 @@ function closewindow()
     window.close();
 }
 
+$(document).ready(function(){
+    $.getJSON("data.json", function (data){
+        var data_v = '';
+        $.each(data, function (key, value){
+            data_v += '<div class="daily_block">';
+            data_v += '<p class="daily_block_label">'+value.label+'</p>';
+            data_v += '<p class="daily_block_count">'+value.count+'</p>';
+            data_v += '<div class="rectangles">';
+            data_v += '<img src="img/rectanglegrey.png" alt="rect">';
+            data_v += '<p class="text1020">1020</p>';
+            data_v += '<img src="img/rectanglered.png" alt="rect">';
+            data_v += '<p class="text1020">1020</p>';
+            data_v += '</div>';
+            data_v += '</div>';
+        });
+        $('#part_1').append(data_v);
+    });
+});
 
+$(document).ready(function(){
+    $.getJSON("data2.json", function (data){
+        var data_v = '';
+        $.each(data, function (key, value){
+            data_v += '<div class="daily_block">';
+            data_v += '<p class="daily_block_label">'+value.label+'</p>';
+            data_v += '<p class="daily_block_count">'+value.count+'</p>';
+            data_v += '<div class="rectangles">';
+            data_v += '<img src="img/rectanglegrey.png" alt="rect">';
+            data_v += '<p class="text1020">1020</p>';
+            data_v += '<img src="img/rectanglered.png" alt="rect">';
+            data_v += '<p class="text1020">1020</p>';
+            data_v += '</div>';
+            data_v += '</div>';
+        });
+        $('#part_2').append(data_v);
+    });
+});
+
+$(document).ready(function (){
+    $('.month__dots').click(function(event){
+        $('.month__dots, .chart__menu').toggleClass('on');
+    });
+});
